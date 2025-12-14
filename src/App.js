f5c969eb4ba46048e69e75ea3b66454ff7440b92
Soswa Tomasz <soswa.tomasz@gmail.com>
2025-12-14 12:06:14 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('Wall-E');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value)
+    }
+
+    let message;
+    if (title.length < 5) {
+        message = "Tutuł jest za krótki. Nagrywają takie filmy?";
+    } else if (title.length < 15) {
+        message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+    } else {
+        message = "Tytuł jest za długi, nikt tego nie zapamięta.";
     }
 
     return (
         <div>
-            <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
+                { title.length > 0 && <div>{message}</div>}
             <input type="text" onChange={handleChange}/>
+
+            <button type="button" onClick={() => alert(title)}>
+                Pokaż tytuł filmu
+            </button>
         </div>
     );
 }
