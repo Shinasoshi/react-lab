98ea43f3a900d50b03ace0b5ce0f7245b28d6a31
Soswa Tomasz <soswa.tomasz@gmail.com>
2026-01-17 11:43:43 +0100

    split files

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import './App.css';
 import "milligram";
 import {useState} from "react";
+import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
 
 function App() {
-    const [title, setTitle] = useState('');
-    const [year, setYear] = useState('');
-
     const [movies, setMovies] = useState([
-        {title: "Wall-E"},
-        {title: "Pulp Fiction"},
-        {title: "Matrix"},
-        {title: "1670"},
+        {title: "Wall-E", year: 1990},
+        {title: "Pulp Fiction", year: 1970},
+        {title: "Matrix", year: 2000},
+        {title: "1670", year: 2024},
     ]);
 
-    function addMovie(event) {
-        event.preventDefault();
-        if (title.length < 5) {
-            return alert('Tytuł jest za krótki');
-        }
-        setMovies([...movies, {title, year}])
-        // props.onMovieSubmit({title, year});
-        setTitle('');
-        setYear('');
-    }
-
-    return <form onSubmit={addMovie}>
+    return (
         <div className="container">
-            <h2>Titles/Years</h2>
-            <ul>
-                {movies.map(movie => <li key={movie.title}>{movie.title} ({movie.year})</li>)}
-            </ul>
-
-            <h2>Add movie</h2>
-            <div>
-                <label>Tytuł</label>
-                <input type="text" value={title} onChange={(event) => setTitle(event.target.value)}/>
-            </div>
-            <div>
-                <label>Rok nagrania</label>
-                <input type="text" value={year} onChange={(event) => setYear(event.target.value)}/>
-            </div>
-            <button>Add a movie</button>
+            <h1>My favourite movies to watch</h1>
+            <MoviesList movies={movies}></MoviesList>
+            <MovieForm onMovieSubmit={(movie) => setMovies([...movies, movie])}/>;
         </div>
-    </form>;
+    )
 }
 
 export default App;
